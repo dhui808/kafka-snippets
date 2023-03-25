ccea1b4e2ce4a5afb4e98515885379f78f737be3
DESKTOP-EV9OUQ1\huich <huichunwuca@yahoo.com>
2023-03-25 13:58:32 -0400

    Send jnson object

---
+++ springboot-kafka/kafkaconsumer/src/main/java/example/kafka/common/Bar.java
@@ -0,0 +1,10 @@
+package example.kafka.common;
+
+import lombok.ToString;
+
+@ToString
+public class Bar {
+	public String bar;
+	public String name;
+	public int id;
+}
--- springboot-kafka/kafkaproducer/src/main/java/example/kafka/producer/RandomNumberProducer.java
+++ springboot-kafka/kafkaproducer/src/main/java/example/kafka/producer/RandomNumberProducer.java
@@ -5,6 +5,8 @@ import org.springframework.kafka.core.*;
 import org.springframework.scheduling.annotation.*;
 import org.springframework.stereotype.*;
 
+import example.kafka.common.Bar;
+
 import java.net.*;
 import java.util.concurrent.*;
 
@@ -15,14 +17,23 @@ public class RandomNumberProducer {
   private static final int MAX = 100_000;
   private static final String topic = "random-number";
   
-  @Autowired private KafkaTemplate<String, String> kafkaTemplate;
+  @Autowired private KafkaTemplate<String, Object> kafkaTemplate;
 
   @Scheduled(fixedRate = 2000)
   public void produce() throws UnknownHostException {
     int random = ThreadLocalRandom.current().nextInt(MIN, MAX);
     int partition = random % 2 == 0? 0 : 1;
     partition = random % 3 == 0? 2 : partition;
-    this.kafkaTemplate.send(topic, partition, "mykey-" + partition, String.valueOf(random));
+    int id = 1;
+    Bar bar = new Bar();
+    bar.bar = "bar" + random;
+    bar.name = "world";
+    bar.id = id++;
+    if (partition < 2 ) {
+    	kafkaTemplate.send(topic, partition, "mykey-" + partition, String.valueOf(random));
+    } else {
+    	kafkaTemplate.send(topic, partition, "mykey-" + partition, bar);
+    }
     // just for logging
     String hostName = InetAddress.getLocalHost().getHostName();
     System.out.println(String.format("%s produced %d partition %d", hostName, random, partition));
