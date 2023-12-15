26b6b0c6f5bac3410e28f3339369a5b62e11e7b8
DESKTOP-EV9OUQ1\huich <huichunwuca@yahoo.com>
2023-12-14 20:11:49 -0500

    Dynamically create topic.

---
+++ spring-kafka-dynamic-topic/README.md
@@ -0,0 +1,15 @@
+### Dynamically Create and Delete Kafka Topic
+
+### Start Docker Kafka (using docker-compose.yml)
+	Start Docker Desktop
+	
+	docker-compose up -d
+
+### Verify Kafka Cluster
+	docker-compose ps
+
+### Start Kafka application
+
+### Stop Docker Kafka
+	docker-compose down
+		   
---
+++ spring-kafka-dynamic-topic/src/main/java/com/example/spring/kafka/MultiTypeMessageListener.java
@@ -0,0 +1,41 @@
+package com.example.spring.kafka;
+
+import java.util.Map;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
+
+public class MultiTypeMessageListener  {
+    private final String id;
+
+    private final String topic;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getTopic() {
+        return this.topic;
+    }
+	public MultiTypeMessageListener(String id, String topic) {
+		this.id = id;
+        this.topic = topic;
+	}
+	
+	@KafkaListener(containerFactory = "greetingKafkaListenerContainerFactory", topicPartitions =
+	{ @TopicPartition(topic = "#{__listener.topic}", partitions = { "0", "1" },
+	     partitionOffsets = @PartitionOffset(partition = "*", initialOffset = "0"))
+	})
+	public void listenGroupGreeting( @Headers Map<String, String> header,
+    		@Header(KafkaHeaders.OFFSET) long os,
+    		@Header(KafkaHeaders.RECEIVED_PARTITION) int receivedpartition,
+    		@Header(KafkaHeaders.GROUP_ID) String group,
+    		Greeting message) {
+
+        System.out.println("Received Json Message in groupId='" + group + "'," + message + ", offset=" + os + ", receivedpartition=" + receivedpartition);
+    }
+}
